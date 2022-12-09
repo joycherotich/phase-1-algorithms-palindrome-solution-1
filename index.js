24658d8417a045db7849f02aa6302ab8c3396085
joy <letimjoy7@gmail.com>
2022-12-09 14:00:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let stringArray = []
+  for(letters of word){
+    stringArray.push(letters)
+  }
+  let stringArray2 = [...stringArray]
+  //iterating from left to right
+  let leftIteration = []
+  for(let i = 0;i < stringArray.length;){
+    leftIteration.push(stringArray.splice(0,1))
+  }
+  //iterating from right to left
+  let rightIteration = []
+  for(let i = 0;i < stringArray2.length;){
+    rightIteration.push(stringArray2.splice(-1,1))
+  }
+  //comparing the results
+  let result = rightIteration.toString() == leftIteration.toString()
+  return result
 }
+
 
 /* 
   Add your pseudocode here
